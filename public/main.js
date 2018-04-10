1162069ab8fbc30ba590fc0b19b8c8fe60a0ee24
Alejandro Velásquez Molina <alejandrovelasquezmolina@gmail.com>
2018-04-10 15:32:19 -0500

    Creando grid con JS

--- public/main.js
+++ public/main.js
@@ -1,1 +1,19 @@
-console.log("Hola mundo!");
+function $(selector){
+   return document.querySelector(selector);
+}
+
+function build_cat(){
+    for(var i = 0;i < 9; i++){
+        var item = build_item(i);
+
+        $("#cat").innerHTML += item;
+            
+
+    }
+}
+
+function build_item(i){
+    return "<div class='cat-element col-xs-4' id='elemento-"+i+"'></div>"
+}
+
+build_cat();
