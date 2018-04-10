29f751fc8b4807a58a3f4469a47cacef3d79ca6c
Alejandro Velásquez Molina <alejandrovelasquezmolina@gmail.com>
2018-04-10 03:02:31 -0500

    Servidor de archivos estaticos

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const http = require("http");
 
 const app = express();
 
+app.use("/static",express.static("public"));
+
+app.get("/", function(req,res){
+   res.sendFile("index.html",{"root": __dirname}) 
+});
+
 var server = http.createServer(app);
 
 server.listen(8000);
